41e2bd46dc94299bab7e622aa9634c8788055186
James Hepburn <jhepb059@uottawa.ca>
2026-02-27 00:10:09 -0500

    Initial Commit

---
+++ OLYMPUS/OLYMPUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OLYMPUSApp.swift
+//  OLYMPUS
+//
+//  Created by James Hepburn on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct OLYMPUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
